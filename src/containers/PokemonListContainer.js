82af725069655e6899c1442c5813f258ee6fe4ae
bd2187 <brandon.dionisio.26@gmail.com>
2019-09-02 12:23:49 -0700

    Remove unused imports

--- src/containers/PokemonListContainer.js
+++ src/containers/PokemonListContainer.js
@@ -1,39 +1,37 @@
 import React from "react";
 import { connect } from "react-redux";
 import {
-  receivePokemon,
-  displayNextBatch,
-  findPokemon,
-  update_queried_pokemon
+    receivePokemon,
+    displayNextBatch,
+    update_queried_pokemon
 } from "../actions";
 import PokemonList from "../components/PokemonList";
-import PokemonTypeContainer from "../containers/PokemonTypeContainer";
 
 class PokemonListContainer extends React.Component {
-  componentDidMount() {
-    this.props.receivePokemon();
-  }
+    componentDidMount() {
+        this.props.receivePokemon();
+    }
 
-  render() {
-    return (
-      <div>
-        <PokemonList
-          pokemon={this.props.pokemon}
-          displayNextBatch={this.props.displayNextBatch}
-          update_queried_pokemon={this.props.update_queried_pokemon}
-        />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <PokemonList
+                    pokemon={this.props.pokemon}
+                    displayNextBatch={this.props.displayNextBatch}
+                    update_queried_pokemon={this.props.update_queried_pokemon}
+                />
+            </div>
+        );
+    }
 }
 
 const mapStateToProps = state => {
-  return {
-    pokemon: state.pokemon
-  };
+    return {
+        pokemon: state.pokemon
+    };
 };
 
 export default connect(
-  mapStateToProps,
-  { receivePokemon, displayNextBatch, update_queried_pokemon }
+    mapStateToProps,
+    { receivePokemon, displayNextBatch, update_queried_pokemon }
 )(PokemonListContainer);
