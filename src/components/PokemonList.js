e44eecfec03470c1e043114f35a043f68cc68e41
Justin Dionisio <justindionisio21@yahoo.com>
2019-09-04 13:10:24 -0700

    Import Loading component. Render the component whenever isFetching is true and added brief descriptions for methods

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PokemonCard from "./PokemonCard";
+import LoadingMessage from './LoadingMessage'
 import { Link } from "react-router-dom";
 
 class PokemonList extends React.Component {
@@ -13,8 +14,14 @@ class PokemonList extends React.Component {
 
     this.handleSearch = this.handleSearch.bind(this);
     this.displayPokemon = this.displayPokemon.bind(this);
+    this.populateMainContent = this.populateMainContent.bind(this)
   }
 
+  /**
+     *  Updates markup in .main-content based on user's query
+     *  @param Object e
+     *  @return
+     */
   handleSearch(e) {
     var value = e.target.value;
 
@@ -35,14 +42,18 @@ class PokemonList extends React.Component {
         }
       });
 
+      // If input value is empty, update queried pokemon,
+      // to contain an empty array so pokemon_to_display
+      // will display
       if (value === "") {
         document.querySelector(".load-more").style.visibility = "visible";
         this.props.update_queried_pokemon([]);
         this.setState({
           error: false
         });
+        // If no pokemon is found based on user's query,
+        // set state.error to true to show "no pokemon found"
       } else if (filtered_pokemon.length === 0) {
-        // document.querySelector(".load-more").style.visibility = "hidden";
         this.props.update_queried_pokemon(filtered_pokemon);
         this.setState({
           error: true
@@ -57,12 +68,14 @@ class PokemonList extends React.Component {
     }, 500);
   }
 
+  /**
+     *  queried_pokmeon takes precedence over pokemon_to_display
+     *  If user searched for pokemon, display the pokemon from
+     *  queried_pokemon. Otherwise, display pokemon from pokemon_to_display
+     *  @param
+     *  @return
+     */
   displayPokemon() {
-    // if user has an input value, display pokemon from queried_pokemon array
-
-    // else
-
-    // display pokemon from pokemon to disply array
     const { queried_pokemon, pokemon_to_display } = this.props.pokemon;
 
     if (queried_pokemon.length > 0) {
@@ -73,6 +86,44 @@ class PokemonList extends React.Component {
       return pokemon_to_display.map(function (pokemon) {
         return <PokemonCard pokemon={pokemon} key={pokemon.entry_number} />;
       });
+    }
+  }
+
+  /**
+    *  Decised whether to display error message, loading interface,
+    *  or pokemon list based on any errors found in props or staste or
+    *  if application is currently fetching pokemon from server
+    *  @param
+    *  @return Object markup
+    */
+  populateMainContent() {
+    const { error: apiError, isFetching } = this.props.pokemon;
+    const { error: queryError } = this.state;
+
+    if (queryError || apiError !== "") {
+      return (
+        <h3 className="error_message">
+          NO POKÉMON MATCHED YOUR SEARCH
+        </h3>
+      );
+    } else if (isFetching) {
+      return <LoadingMessage />
+    } else {
+      return (
+        <>
+          <div className="pokemon-container">
+            {this.displayPokemon()}
+          </div>
+          <div className="load">
+            <button
+              className="load-more"
+              onClick={this.props.displayNextBatch}
+            >
+              Load More Pokémon
+        </button>
+          </div>
+        </>
+      )
     }
   }
 
@@ -97,17 +148,7 @@ class PokemonList extends React.Component {
           </form>
         </div>
         <div className="main-content">
-          {this.state.error ? (
-            <h3 className="error_message">NO POKÉMON MATCHED YOUR SEARCH</h3>
-          ) : (
-              <div className="pokemon-container">{this.displayPokemon()}</div>
-            )}
-
-          <div className="load">
-            <button className="load-more" onClick={this.props.displayNextBatch}>
-              Load More Pokémon
-            </button>
-          </div>
+          {this.populateMainContent()}
         </div>
       </div>
     );
