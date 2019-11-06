1dce6fcd78c206e7e4e77047583f377ef8378cce
Justin Dionisio <justindionisio21@yahoo.com>
2019-11-05 19:56:49 -0800

    remove console log

--- src/components/LoadingMessage.js
+++ src/components/LoadingMessage.js
@@ -3,7 +3,7 @@ import React from "react";
 class LoadingMessage extends React.Component {
 	constructor(props) {
 		super(props);
-		console.log('i am on develop branch')
+
 		this.enableMessage = this.enableMessage.bind(this);
 
 		this.state = {
